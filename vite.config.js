41e29bb4f9bca15015ac603171bb6d762a790562
LE PHU QUY <155272789+MooreRaby@users.noreply.github.com>
2024-05-06 03:46:05 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+dotenv.config();
+// eslint-disable-next-line no-undef
+var endpoint = process.env.endpoint || "http://localhost:5000"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": endpoint || "http://localhost:5000",
+      "/uploads/": endpoint || "http://localhost:5000",
     },
   },
 });
